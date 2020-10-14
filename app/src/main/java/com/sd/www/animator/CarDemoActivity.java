45735a814090758843fe98b17d77c31766b7f68e
zjun <565061763@qq.com>
2020-10-14 18:26:07 +0800

    androidx

--- app/src/main/java/com/sd/www/animator/CarDemoActivity.java
+++ app/src/main/java/com/sd/www/animator/CarDemoActivity.java
@@ -2,7 +2,7 @@ package com.sd.www.animator;
 
 import android.animation.Animator;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
--- app/src/main/java/com/sd/www/animator/RocketDemoActivity.java
+++ app/src/main/java/com/sd/www/animator/RocketDemoActivity.java
@@ -3,7 +3,7 @@ package com.sd.www.animator;
 import android.animation.Animator;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
